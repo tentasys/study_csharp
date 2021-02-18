190343c8e9033d12f8830b49ec5d65726db0f745
HeeJung Kang <tentasys@gmail.com>
2021-02-18 23:10:14 +0900

    20120218

---
+++ Appendix_StringModify/Appendix_StringModify/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Appendix_StringModify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("ToLower() : '{0}'", "ABC".ToLower());
+            WriteLine("ToUpper() : '{0}'", "abc".ToUpper());
+
+            WriteLine("Insert() : '{0}'", "Happy Friday!".Insert(5, " Sunny"));
+            WriteLine("Remove() : '{0}'", "I Don't Love You.".Remove(2, 6));
+
+            WriteLine("Trim() : '{0}'", " No Spaces ".Trim());
+            WriteLine("TrimStart() : '{0}'", " No Spaces ".TrimStart());
+            WriteLine("TrimEnd() : '{0}'", " No Spaces ".TrimEnd());
+        }
+    }
+}
