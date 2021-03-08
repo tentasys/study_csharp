dda18028d68d9820199ab5a79005e47218cd89fb
HeeJung Kang <tentasys@gmail.com>
2021-03-09 00:14:58 +0900

    20210309

--- Appendix_StringFormatDatetime/Appendix_StringFormatDatetime/Program.cs
+++ Appendix_StringFormatDatetime/Appendix_StringFormatDatetime/Program.cs
@@ -12,7 +12,7 @@ namespace Appendix_StringFormatDatetime
     {
         static void Main(string[] args)
         {
-            DateTime dt = new DateTime(2019, 11, 3, 23, 18, 22);
+            DateTime dt = new DateTime(2021, 03, 09, 00, 02, 48);
 
             WriteLine("12시간 형식 : {0:yyyy-MM-dd tt hh:mm:ss (ddd)}", dt);
             WriteLine("24시간 형식 : {0:yyyy-MM-dd HH:mm:ss (dddd)}", dt);
@@ -25,7 +25,17 @@ namespace Appendix_StringFormatDatetime
 
             CultureInfo ciEn = new CultureInfo("en-US");
             WriteLine();
-           
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciEn));
+            WriteLine(dt.ToString("yyyy-MM-dd HH:mm:ss (dddd)", ciEn));
+            WriteLine(dt.ToString(ciEn));
+
+            //cultureinfo 이용한 초기화
+            //어차피 시간을 인수로 받아서 now 쓰지 않는한 똑같을듯. 관건은 현재 시간을 받아오는 것! 
+            long ticks = new DateTime(2021, 03, 09, 00, 02, 48, new CultureInfo("en-US", false).Calendar).Ticks;
+            dt = new DateTime(ticks);
+
+            WriteLine("12시간 형식 : {0:yyyy-MM-dd tt hh:mm:ss (ddd)}", dt);
+            WriteLine("24시간 형식 : {0:yyyy-MM-dd HH:mm:ss (dddd)}", dt);
         }
     }
 }
