39ca5fb18794a76d4fd562dcceb8d98e36a62397
HeeJung Kang <tentasys@gmail.com>
2021-03-08 23:58:12 +0900

    20210308 study

---
+++ Appendix_StringFormatBasic/StringFormatBasic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace StringFormatBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fmt = "{0, -20}{1, -15}{2, 30}";
+
+            WriteLine(fmt, "Publisher", "Author", "Title");
+            WriteLine(fmt, "Marvle", "Stan Lee", "Iron Man");
+            WriteLine(fmt, "Hanbit", "Sangyun Park", "C# 7.0 Programming");
+            WriteLine(fmt, "Prentice Hall", "K&R", "The C Programming Language");
+        }
+    }
+}
---
+++ Appendix_StringFormatDatetime/Appendix_StringFormatDatetime/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Globalization;
+
+namespace Appendix_StringFormatDatetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2019, 11, 3, 23, 18, 22);
+
+            WriteLine("12시간 형식 : {0:yyyy-MM-dd tt hh:mm:ss (ddd)}", dt);
+            WriteLine("24시간 형식 : {0:yyyy-MM-dd HH:mm:ss (dddd)}", dt);
+
+            CultureInfo ciKo = new CultureInfo("ko-KR");
+            WriteLine();
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciKo));
+            WriteLine(dt.ToString("yyyy-MM-dd HH:mm:ss (dddd)", ciKo));
+            WriteLine(dt.ToString(ciKo));
+
+            CultureInfo ciEn = new CultureInfo("en-US");
+            WriteLine();
+           
+        }
+    }
+}
