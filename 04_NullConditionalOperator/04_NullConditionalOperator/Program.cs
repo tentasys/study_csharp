dc54c55a24b95576731cc2c24fd8e0194a3780dc
HeeJung Kang <tentasys@gmail.com>
2021-03-24 23:45:57 +0900

    20210324

---
+++ 04_NullConditionalOperator/04_NullConditionalOperator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _04_NullConditionalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = null;
+            a?.Add("야구");
+            a?.Add("축구");
+            //a가 null이므로 Count : 이외에는 아무것도 출력되지 않음
+            WriteLine($"count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+
+            a = new ArrayList();
+            a?.Add("야구");
+            a?.Add("축구");
+            WriteLine($"count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
+        }
+    }
+}
