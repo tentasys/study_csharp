1b53436e5a7fe69fe98960c8372ea4771af7252b
HeeJung Kang <tentasys@gmail.com>
2021-03-31 22:29:04 +0900

    20210331

---
+++ 05_ForEach/05_ForEach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 0, 1, 2, 3, 4 };
+
+            foreach(int a in arr)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
---
+++ 05_InfiniteWhile/05_InfiniteWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_InfiniteWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine(i++);
+            }
+        }
+    }
+}
