cd84a49d4e5804e5381c64d6374418520195bada
HeeJung Kang <tentasys@gmail.com>
2021-06-09 22:10:27 +0900

    20210609

---
+++ 07_BasicClass/07_BasicClass/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _07_BasicClass
+{
+    class Cat
+    {
+        public string Name;
+        public string color;
+
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} : 야옹");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat()
+            {
+                color = "하얀색",
+                Name = "키티"
+            };
+        
+            
+            kitty.Meow();
+            Console.WriteLine($"{kitty.Name} : {kitty.color}");
+
+            Cat nero = new Cat();
+            nero.color = "검은색";
+            nero.Name = "네로";
+            nero.Meow();
+            Console.WriteLine($"{nero.Name} : {nero.color}");
+        }
+    }
+}
