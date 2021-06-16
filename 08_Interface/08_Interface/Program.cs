ccdc067a7951d3e83593b1f3a9710c5d2005050b
HeeJung Kang <tentasys@gmail.com>
2021-06-17 07:40:36 +0900

    20210617

---
+++ 08_Interface/08_Interface/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace _08_Interface
+{
+    interface ILogger
+    {
+        void WriteLog(string mesage);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), message);
+        }
+    }
+    class FileLogger : ILogger
+    {
+        private StreamWriter writer;
+        public FileLogger(string path)
+        {
+            writer = File.CreateText(path);
+            writer.AutoFlush = true;
+        }
+
+        public void WriteLog(string message)
+        {
+            writer.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), message);
+        }
+    }
+    class ClimateMonitor
+    {
+        private ILogger logger;
+        public ClimateMonitor(ILogger logger)
+        {
+            this.logger = logger;
+        }
+        public void start()
+        {
+            while(true)
+            {
+                Console.Write("온도를 입력해 주세요 : ");
+                string temperature = Console.ReadLine();
+                if (temperature == "") break;
+
+                logger.WriteLog("현재 온도 : " + temperature);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClimateMonitor monitor = new ClimateMonitor(new FileLogger("MyLog.txt"));
+            //ClimateMonitor monitor = new ClimateMonitor(new ConsoleLogger());
+
+            monitor.start();
+        }
+    }  
+}
