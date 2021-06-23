1c2d4b29a342927d6270e03dcaa9488e34880674
HeeJung Kang <tentasys@gmail.com>
2021-06-23 23:14:42 +0900

    20210623

---
+++ 11_ConstraintsOnTypeParameters/11_ConstraintsOnTypeParameters/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_ConstraintsOnTypeParameters
+{
+    class StructArray<T> where T : struct
+    {
+        public T[] Array { get; set; }
+        public StructArray(int size)
+        {
+            Array = new T[size];
+        }
+        public int Length
+        {
+            get { return Array.Length; }
+        }
+    }
+    class RefArray<T> where T : class
+    {
+        public T[] Array { get; set; }
+        public RefArray(int size)
+        {
+            Array = new T[size];
+        }
+    }
+    class Base { 
+        public Base()
+        {
+            Console.WriteLine("Base Created");
+        }
+    }
+    class Derived : Base { 
+        public Derived()
+        {
+            Console.WriteLine("Derived Created");
+        }
+    }
+    class BaseArray<U> where U : Base
+    {
+        public U[] Array { get; set; }
+        public BaseArray(int size)
+        {
+            Array = new U[size];
+        }
+        public void CopyArray<T> (T[] source) where T : U
+        {
+            source.CopyTo(Array, 0);
+            Console.WriteLine("Copied");
+        }
+    }
+
+    interface ITest
+    {
+        void Write();
+    }
+    class TestClass : ITest
+    {
+        public void Write()
+        {
+            Console.WriteLine("TestClass");
+        }
+    }
+
+    class Program
+    {
+        public static T CreateInstance<T>() where T : new()
+        {
+            return new T();
+        }
+
+        public static void ShowMe<T>(T obj) where T : ITest
+        {
+            obj.Write();
+        }
+
+        static void Main(string[] args)
+        {
+            StructArray<int> a = new StructArray<int>(3);
+            a.Array[0] = 0;
+            a.Array[1] = 1;
+            a.Array[2] = 2;
+            for (int i = 0; i < 3; i++)
+                Console.Write($"{a.Array[i]} ");
+            Console.WriteLine();
+
+            RefArray<StructArray<double>> b = new RefArray<StructArray<double>>(3);
+            b.Array[0] = new StructArray<double>(5);
+            b.Array[1] = new StructArray<double>(10);
+            b.Array[2] = new StructArray<double>(1005);
+            for (int i = 0; i < 3; i++)
+                Console.Write($"{b.Array[i].Length} ");
+            Console.WriteLine();
+
+            BaseArray<Base> c = new BaseArray<Base>(3);
+            c.Array[0] = new Base();
+            c.Array[1] = new Derived();
+            c.Array[2] = CreateInstance<Base>();
+            Console.WriteLine();
+
+            BaseArray<Derived> d = new BaseArray<Derived>(3);
+            d.Array[0] = new Derived();     //Base형식은 여기에 할당 불가
+            d.Array[1] = CreateInstance<Derived>();
+            d.Array[2] = CreateInstance<Derived>();
+            Console.WriteLine();
+
+            BaseArray<Derived> e = new BaseArray<Derived>(3);
+            e.CopyArray<Derived>(d.Array);
+            Console.WriteLine();
+
+            ShowMe<TestClass>(new TestClass());
+        }
+    }
+}
---
+++ 11_EnumerableGeneric/11_EnumerableGeneric/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace _11_EnumerableGeneric
+{
+    class MyList<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        private T[] array;
+        int position = -1;  //컬렉션의 현재 위치를 다루는 변수
+        public MyList()
+        {
+            array = new T[3];
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                if (index >= array.Length)
+                {
+                    Array.Resize<T>(ref array, index + 1);
+                    Console.WriteLine($"Array Resized : {array.Length}");
+                }
+                array[index] = value;
+            }
+        }
+
+        public T Current   //현재 위치의 요소를 반환
+        {
+            get
+            {
+                return array[position];
+            }
+        }
+        object IEnumerator.Current
+        {
+            get { return array[position]; }
+        }
+
+        public bool MoveNext()      //다음 위치의 요소로 이동
+        {
+            if (position == array.Length - 1)
+            {
+                Reset();
+                return false;
+            }
+
+            position++;
+            return (position < array.Length);
+        }
+
+        public void Reset()     //요소의 위치를 첫 요소의 앞으로 옮김
+        {
+            position = -1;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < array.Length; i++)
+                yield return (array[i]);
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < array.Length; i++)
+                yield return (array[i]);
+        }
+        public void Dispose()
+        { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> str_list = new MyList<string>();
+            str_list[0] = "abc";
+            str_list[1] = "def";
+            str_list[2] = "ghi";
+            str_list[3] = "jkl";
+            str_list[4] = "mno";
+
+            foreach (string e in str_list)
+                Console.WriteLine(e);
+
+            MyList<int> int_list = new MyList<int>();
+            for (int i = 0; i < 5; i++)
+                int_list[i] = i;
+
+            foreach (int e in int_list)
+                Console.WriteLine(e);
+        }
+    }
+}
