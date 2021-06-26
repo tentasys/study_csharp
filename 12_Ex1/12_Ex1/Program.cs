752c6e3ba7e8106e0bb44d0b47d20ac5c3a7bd1d
HeeJung Kang <tentasys@gmail.com>
2021-06-26 16:52:19 +0900

    20210626

---
+++ 12_Ex1/12_Ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _12_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] arr = new int[10];
+
+                for (int i = 0; i < 10; i++)
+                    arr[i] = i;
+                for (int i = 0; i < 11; i++)
+                    Console.WriteLine(arr[i]);
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ 12_ThrowExpression/12_ThrowExpression/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _12_ThrowExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int? a = null;
+                int b = a ?? throw new ArgumentNullException();
+            }
+            catch(ArgumentNullException e)
+            {
+                Console.WriteLine(e);
+            }
+            try
+            {
+                int[] array = new[] { 1, 2, 3 };
+                int index = 4;
+                int value = array[index >= 0 && index < 3 ? index : throw new IndexOutOfRangeException()];
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
