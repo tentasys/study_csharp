b000c799ecef0129e01c5680041f41026b52efa0
HeeJung Kang <tentasys@gmail.com>
2021-06-28 22:41:21 +0900

    20210628

---
+++ 13_Delegate/13_Delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _13_Delegate
+{
+    delegate int MyDelegate(int a, int b);
+    class Calculator
+    {
+        //인스턴스 메소드
+        public int Plus(int a, int b) { return a + b; }
+        //스태틱 메소드
+        public static int Minus(int a, int b) { return a - b; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator Calc = new Calculator();
+            MyDelegate Callback;
+
+            Callback = new MyDelegate(Calc.Plus);
+            Console.WriteLine(Callback(3, 4));
+
+            Callback = new MyDelegate(Calculator.Minus);
+            Console.WriteLine(Callback(5, 2));
+        }
+    }
+}
