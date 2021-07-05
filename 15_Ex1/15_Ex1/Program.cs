269de59b56c59bb09c45d8dc37a9261bdc36c70a
HeeJung Kang <tentasys@gmail.com>
2021-07-06 07:36:11 +0900

    20210706

---
+++ 15_Ex1/15_Ex1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _15_Ex1
+{
+    class Car
+    {
+        public int Cost { get; set; }
+        public int MaxSpeed { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars =
+            {
+                new Car(){Cost=56, MaxSpeed=120},
+                new Car(){Cost=70, MaxSpeed=150},
+                new Car(){Cost=45, MaxSpeed=180},
+                new Car(){Cost=32, MaxSpeed=200},
+                new Car(){Cost=82, MaxSpeed=280}
+            };
+
+            var selected = from car in cars
+                           where car.Cost >= 50 && car.MaxSpeed >= 150
+                           select new { Cost = car.Cost, MaxSpeed = car.MaxSpeed };
+
+            foreach(var car in selected)
+            {
+                Console.WriteLine($"Cost : {car.Cost}, MaxSpeed : {car.MaxSpeed}");
+            }
+        }
+    }
+}
---
+++ 15_Ex2/15_Ex2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _15_Ex2
+{
+    class Car
+    {
+        public int Cost { get; set; }
+        public int MaxSpeed { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars =
+            {
+                new Car(){Cost = 56, MaxSpeed = 120},
+                new Car(){Cost = 70, MaxSpeed = 150},
+                new Car(){Cost = 45, MaxSpeed = 180},
+                new Car(){Cost = 32, MaxSpeed = 200},
+                new Car(){Cost = 82, MaxSpeed = 280}
+            };
+
+            var selected = from car in cars
+                           where car.Cost < 60
+                           orderby car.Cost
+                           select car;
+
+            foreach(var car in selected)
+            {
+                Console.WriteLine($"Cost : {car.Cost}, MaxSpeed : {car.MaxSpeed}");
+            }
+        }
+    }
+}
