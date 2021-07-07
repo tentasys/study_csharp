70e8bf8671906b7059b15b0d36a8874f09946734
HeeJung Kang <tentasys@gmail.com>
2021-07-07 22:17:03 +0900

    20210707

---
+++ 16_DynamicInstance/16_DynamicInstance/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reflection;
+
+namespace _16_DynamicInstance
+{
+    class Profile
+    {
+        private string name;
+        private string phone;
+        public Profile()
+        {
+            name = ""; phone = "";
+        }
+        public Profile(string name, string phone)
+        {
+            this.name = name;
+            this.phone = phone;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{name}, {phone}");
+        }
+        public void Print2(string msg)
+        {
+            Console.WriteLine($"{name}'s msg : {msg}");
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Phone
+        {
+            get { return name; }
+            set { phone = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = Type.GetType("_16_DynamicInstance.Profile");
+            MethodInfo methodInfo = type.GetMethod("Print");
+            PropertyInfo nameProperty = type.GetProperty("Name");
+            PropertyInfo phoneProperty = type.GetProperty("Phone");
+
+            object profile = Activator.CreateInstance(type, "강희정", "123-4567");
+            methodInfo.Invoke(profile, null);
+
+            profile = Activator.CreateInstance(type);
+            nameProperty.SetValue(profile, "테르", null);
+            phoneProperty.SetValue(profile, "1111", null);
+
+            Console.WriteLine("{0}, {1}", nameProperty.GetValue(profile, null), phoneProperty.GetValue(profile, null));
+
+            methodInfo = type.GetMethod("Print2");
+            string[] param = { "test"};
+            methodInfo.Invoke(profile, param);
+        }
+    }
+}
---
+++ 16_GetType/16_GetType/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace _16_GetType
+{
+    class Test
+    {
+        int a = 0;
+        int b = 2;
+
+        public Test()
+        {
+            Console.WriteLine("plain");
+        }
+        public Test(String msg)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    class Program
+    {
+        static void PrintInterfaces(Type type)
+        {
+            Console.WriteLine("------ Interfaces ------");
+            Type[] interfaces = type.GetInterfaces();
+            foreach (Type i in interfaces)
+                Console.WriteLine("Name : {0}", i.Name);
+            Console.WriteLine();
+        }
+        static void PrintFields(Type type)
+        {
+            Console.WriteLine("------ Fields ------");
+            FieldInfo[] fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance);
+
+            foreach(FieldInfo field in fields)
+            {
+                String accessLevel = "protected";
+                if (field.IsPublic) accessLevel = "public";
+                else if (field.IsPrivate) accessLevel = "private";
+
+                Console.WriteLine("Access : {0}, Type : {1}, Name : {2}", accessLevel, field.FieldType.Name, field.Name);
+            }
+            Console.WriteLine();
+        }
+        static void PrintMethods(Type type)
+        {
+            Console.WriteLine("------ Methods ------");
+
+            MethodInfo[] methods = type.GetMethods();
+            foreach(MethodInfo method in methods)
+            {
+                Console.Write("Type : {0}, Name : {1}, Parameter : ", method.ReturnType.Name, method.Name);
+
+                ParameterInfo[] args = method.GetParameters();
+                for(int i=0; i<args.Length; i++)
+                {
+                    Console.Write("{0}", args[i].ParameterType.Name);
+                    if (i < args.Length - 1) Console.Write(",");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static void PrintProperties(Type type)
+        {
+            Console.WriteLine("------ Properties ------");
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (PropertyInfo property in properties)
+                Console.WriteLine("Type : {0}, Name : {1}", property.PropertyType.Name, property.Name);
+            Console.WriteLine();
+        }
+        static void PrintConstructors(Type type)
+        {
+            Console.WriteLine("------ Constructors ------");
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach (ConstructorInfo c in constructors)
+            {
+                ParameterInfo[] args = c.GetParameters();
+                Console.WriteLine("Parameter : ");
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.Write("{0}", args[i].ParameterType.Name);
+                    if (i < args.Length - 1) Console.Write(",");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static void PrintEvents(Type type)
+        {
+            Console.WriteLine("------ Events ------");
+            EventInfo[] events = type.GetEvents();
+            
+            foreach(EventInfo e in events)
+            {
+                Console.WriteLine("Name : {0}", e.Name);
+            }
+            Console.WriteLine();
+        }
+        static void PrintGenericArguments(Type type)
+        {
+            Console.WriteLine("------  ------");
+        }
+        static void PrintMembers(Type type)
+        {
+            Console.WriteLine("------  ------");
+        }
+        static void PrintNestedTypes(Type type)
+        {
+            Console.WriteLine("------  ------");
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Type type = a.GetType();
+
+            PrintInterfaces(typeof(int));
+            PrintFields(Type.GetType("System.Int32"));
+            PrintProperties(type);
+            PrintMethods(type);
+            PrintConstructors(new Test().GetType());
+        }
+    }
+}
