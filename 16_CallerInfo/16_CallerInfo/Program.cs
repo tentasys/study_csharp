9a32c8971610dff6720a4df24bb7af2687b264b6
HeeJung Kang <tentasys@gmail.com>
2021-07-08 22:22:16 +0900

    20210708

---
+++ 16_CallerInfo/16_CallerInfo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace _16_CallerInfo
+{
+    public static class Trace
+    {
+        public static void WriteLine(string message, [CallerFilePath] string file = "", [CallerLineNumber] int line = 0, [CallerMemberName] string member = "")
+        {
+            Console.WriteLine($"{file}(line:{line}) {member}: {message}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.WriteLine("즐거운 프로그래밍!");
+        }
+    }
+}
