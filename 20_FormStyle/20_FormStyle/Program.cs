9cf20d818f2774c15fc93927f0aa55366bd57573
HeeJung Kang <tentasys@gmail.com>
2021-07-16 22:00:52 +0900

    20210716

---
+++ 20_FormStyle/20_FormStyle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _20_FormStyle
+{
+    class Program : Form
+    {
+        static void Main(string[] args)
+        {
+            Program form = new Program();
+
+            form.Width = 400;
+            form.MouseDown += new MouseEventHandler(form_MouseDown);
+
+            Application.Run(form);
+        }
+        static void form_MouseDown(object sender, MouseEventArgs e)
+        {
+            Form form = (Form)sender;
+
+            if(e.Button == MouseButtons.Left)
+            {
+                form.MaximizeBox = true;
+                form.MinimizeBox = true;
+                form.Text = "최소화/최대화 버튼이 활성화되었습니다.";
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                form.MaximizeBox = false;
+                form.MinimizeBox = false;
+                form.Text = "최소화/최대화 버튼이 비활성화되었습니다.";
+            }
+        }
+    }
+}
